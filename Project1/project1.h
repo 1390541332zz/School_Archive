51ccd8761fbb4ed1fd4c9efcf497098cc83d8da8
jacoblambda <jacobabel@live.com>
2017-02-05 01:07:01 -0500

    Complete Project 1

---
+++ Project1/project1.h
@@ -0,0 +1,16 @@
+#ifndef PROJECT1_H
+#define PROJECT1_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using std::string;
+using std::stoi;
+using std::ifstream;
+using std::ofstream;
+using std::endl;
+
+int blackjack(string fileIn, string fileOut);
+
+#endif /* PROJECT1_H */
