2b65a47e0dbb35b294524128ef43061289a11bd3
Patrick Schaumont <schaum@vt.edu>
2018-01-18 09:16:10 -0500

    initial version

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+int main(void) {
+    volatile uint32_t i;
+
+    // Stop watchdog timer
+    WDT_A_hold(WDT_A_BASE);
+
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+
+    while (1) {
+
+        GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
+
+        // Delay through empty loop
+        for (i = 50000; i > 0; i--)   ;
+    }
+}
