9db1b47014b4cc7ac6cad682e5aaa3ff36cd552f
Patrick Schaumont <schaum@vt.edu>
2018-01-22 20:47:22 -0500

    initial

---
+++ register-version/main.c
@@ -0,0 +1,18 @@
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+int main(void) {
+    volatile uint32_t i;
+
+    // Stop watchdog timer
+    WDT_A_hold(WDT_A_BASE);
+
+    P1DIR = 1;  // P1.0 as output
+    P1REN = 2;  // P1.1 as input with pullup
+
+    while (1) {
+        if (P1IN & 2)
+            P1OUT = P1OUT & ~1;
+        else
+            P1OUT = P1OUT |  1;
+    }
+}
