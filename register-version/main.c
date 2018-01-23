8c9559c76d164530614d40cd16ac3de8379d05d9
Patrick Schaumont <schaum@vt.edu>
2018-01-23 14:18:34 -0500

    set pull-up resistor

--- register-version/main.c
+++ register-version/main.c
@@ -7,7 +7,9 @@ int main(void) {
     WDT_A_hold(WDT_A_BASE);
 
     P1DIR = 1;  // P1.0 as output
-    P1REN = 2;  // P1.1 as input with pullup
+
+    P1REN = 2;  // P1.1 as input with pullup/pulldown resistor
+    P1OUT = 2;  // select pull-up
 
     while (1) {
         if (P1IN & 2)
