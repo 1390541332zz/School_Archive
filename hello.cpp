339592cc360ac6f367c9a717834a22dd408b75c4
Jacob Abel <jacobabel@live.com>
2018-01-24 12:13:24 -0500

    Changed the message printed

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,6 @@ int main(int argc, char** argv)
 {
 	std::cout << "Hello World!" << std::endl;
 	std::cout << "My name is Jacob Abel" << std::endl;
+	std::cout << "This program is for Exercise 03" << std::endl;
 	return 0;
 }
