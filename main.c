332da5d322af75a78080ae67f9fab6519a0a98ed
Patrick Schaumont <schaum@vt.edu>
2018-01-26 13:01:00 -0500

    solution for left side

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
 #define LEFT_BUTTON    BIT1
-#define RIGHT_BUTTON   BIT4
+
+// THIS MACRO HAS TO BE DEFINED BY YOU
+#define RIGHT_BUTTON   
 
 #define LED1           BIT0
-#define LED2_RED       BIT0
-#define LED2_GREEN     BIT1
-#define LED2_BLUE      BIT2
+
+// THIS MACRO HAS TO BE DEFINED BY YOU
+#define LED2_RED       
+
+// THIS MACRO HAS TO BE DEFINED BY YOU
+#define LED2_GREEN     
+
+// THIS MACRO HAS TO BE DEFINED BY YOU
+#define LED2_BLUE      
 
 #define PUSHED 0
 
@@ -16,7 +24,33 @@ void main(void) {
 
   WDT_A_hold(WDT_A_BASE);
 
-  // your solution goes here
+  // Set the LED pins as output
+  // The dafault is input. In order to turn into output, 
+  // we need to set the corresponding bit to 1
+  P1DIR = LED1;
 
+  // Set the button pins as input
+  // The default for PxDIR is input, so we do not need to 
+  // change that. We need to specify if the input needs a 
+  // pull (up or down) resistor. Based on figure 29 of 
+  // board document, buttons will need pull up resistors
+  // Based on Table 12-1 of microcontroller document, 
+  // both REN and OUT bits should be set to 1 to achieve this
+  P1REN = LEFT_BUTTON;
+  P1OUT = LEFT_BUTTON;
 
+  unsigned char left_button_prev, left_button_cur;
+
+  while(1) {
+
+      left_button_cur = (P1IN & LEFT_BUTTON);
+
+      if((left_button_cur  == PUSHED) && 
+	     (left_button_prev != PUSHED)) {
+          P1OUT ^= LED1; //toggle the LED
+      }
+
+      left_button_prev = left_button_cur;
+  }
+  
 }
