e23081a2ce8e3f2eab5639d1fbc28257f59f32cf
Jacob Abel <jacobabel@live.com>
2018-01-29 15:24:51 -0500

    Increase CMakeList Compile Strictness & Complete lexer

--- lexer.cpp
+++ lexer.cpp
@@ -7,9 +7,15 @@ static std::string const delim_chars = "#()\",=";
 
 #define SYNTAX_CHECK(TOKLIST, TOKSTACK, CURLINE) do {                                                                  \
     if (!TOKSTACK.empty() && (TOKSTACK.top().type() == STRING_DELIM)) {                                                \
+        while (!TOKLIST.empty() && (TOKLIST.back().type() != STRING_DELIM)) {                                          \
+            TOKLIST.pop_back();                                                                                        \
+        }                                                                                                              \
         TOKLIST.emplace_back(ERROR, CURLINE, err_mismatchQuote + std::to_string(CURLINE));                             \
         return TOKLIST;                                                                                                \
     } else if (!TOKSTACK.empty()) {                                                                                    \
+        while (!TOKLIST.empty() && (TOKLIST.back().type() != OPEN_PAREN)) {                                            \
+            TOKLIST.pop_back();                                                                                        \
+        }                                                                                                              \
         TOKLIST.emplace_back(ERROR, CURLINE, err_mismatchParen + std::to_string(CURLINE));                             \
         return TOKLIST;                                                                                                \
     }                                                                                                                  \
@@ -27,7 +33,6 @@ TokenList tokenize(std::istream & is)
         std::size_t delim_offset = std::string::npos;
         switch(c) {
             case '#':
-                SYNTAX_CHECK(toklist, tokstack, curline);
                 is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
                 is.unget();
                 break;
@@ -51,10 +56,11 @@ TokenList tokenize(std::istream & is)
                 toklist.emplace_back(CLOSE_PAREN, curline);
                 break;
             case '\"':
-               if (!tokstack.empty() && (tokstack.top().line() != curline)) {
+                if (!tokstack.empty() && (tokstack.top().line() != curline)) { 
                     toklist.emplace_back(ERROR, curline, err_mismatchQuote + std::to_string(curline));
                     return toklist;
                 } else if (!tokstack.empty()) {
+                    if (toklist.back().type() == STRING_DELIM) toklist.emplace_back(STRING, curline, "");
                     tokstack.pop();
                 } else {
                     tokstack.emplace(STRING_DELIM, curline);
@@ -63,20 +69,21 @@ TokenList tokenize(std::istream & is)
                 break;
             case '\n':
                 SYNTAX_CHECK(toklist, tokstack, curline);
-                if (toklist.empty() || (toklist.back().type() != EOL)) toklist.emplace_back(EOL, curline);
+                if (!toklist.empty() && (toklist.back().type() != EOL)) toklist.emplace_back(EOL, curline);
                 ++curline; 
                 break;
             default:
                 if (std::isspace(c)) break;
                 is.unget();
-                if (!tokstack.empty() && tokstack.top().type() == STRING_DELIM) {
+                if (!tokstack.empty() && (tokstack.top().type() == STRING_DELIM)) {
                     std::getline(is, str);
-                    delim_offset = str.find_first_of('\"');
+                    delim_offset = str.find_first_of("\"#");
+                    is.unget();
+                    if (is.peek() != '\n') is.ignore();
                 } else {
                     is >> str;
                     delim_offset = str.find_first_of(delim_chars);
-                }
-                
+                }            
                 if (delim_offset != std::string::npos) {
                     is.seekg(delim_offset - str.size(), std::ios_base::cur);
                     str = str.substr(0, delim_offset);
@@ -85,5 +92,6 @@ TokenList tokenize(std::istream & is)
                 break;
         }
     }
+    if (!toklist.empty() && (toklist.back().type() != EOL))  toklist.emplace_back(EOL, curline); 
     return toklist;
 }
