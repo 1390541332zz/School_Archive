0640586747787509dcebe51b9f877905f301af69
lnazhand <leyla@vt.edu>
2018-01-29 11:21:02 -0500

    replaced numbers with named masks

--- register-version/main.c
+++ register-version/main.c
@@ -6,15 +6,15 @@ int main(void) {
     // Stop watchdog timer
     WDT_A_hold(WDT_A_BASE);
 
-    P1DIR = 1;  // P1.0 as output
+    P1DIR = BIT0;  // P1.0 as output
 
-    P1REN = 2;  // P1.1 as input with pullup/pulldown resistor
-    P1OUT = 2;  // select pull-up
+    P1REN = BIT1;  // P1.1 as input with pullup/pulldown resistor
+    P1OUT = BIT1;  // select pull-up
 
     while (1) {
         if (P1IN & 2)
-            P1OUT = P1OUT & ~1;
+            P1OUT = P1OUT & ~BIT0;
         else
-            P1OUT = P1OUT |  1;
+            P1OUT = P1OUT |  BIT0;
     }
 }
