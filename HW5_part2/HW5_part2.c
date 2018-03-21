e6483c874d67ee1119d0fb944abd8355da0fdf78
Jacob Abel <jacobabel@live.com>
2018-03-20 21:24:33 -0400

    Complete HW5-2

--- HW5_part2/HW5_part2.c
+++ HW5_part2/HW5_part2.c
@@ -1,11 +1,13 @@
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+#include <stdio.h>
 #include <ti/grlib/grlib.h>
 #include "LcdDriver/Crystalfontz128x128_ST7735.h"
 #include <LED_HAL.h>
 #include <Buttons_HAL.h>
 #include <Timer_HAL.h>
 #include <ADC_HAL.h>
-#include <stdio.h>
+
+#define ENTR_DELAY 6
 
 Graphics_Context g_sContext;
 
@@ -31,10 +33,10 @@ void LCDDrawChar(unsigned row, unsigned col, int8_t c) {
 }
 
 
+
 int main(void) {
     WDT_A_hold(WDT_A_BASE);
     unsigned vx, vy;
-    char buf[10];
 
     initADC_Multi();
     initJoyStick();
@@ -47,10 +49,10 @@ int main(void) {
     StartOneShot1sTimer();
     startADC();
 
-    // The number of zeros and ones produced on the pseudo-random string
-    int ones = 0;
-    int zeros = 0;
-
+    uint_fast8_t i = 0;
+    uint_fast8_t x = 0;
+    uint_fast8_t y = 63;
+    uint_fast8_t randval = 0;
     while (1) {
         if (Booster_Top_Button_Pushed())
             Toggle_Launchpad_Left_LED();
@@ -58,19 +60,24 @@ int main(void) {
         getSampleJoyStick(&vx, &vy);
 
         int randBit = (vx%2) ^ (vy%2);
-        if (randBit)
-            ones++;
-        else
-            zeros++;
 
-        int percent = (ones * 100) / (ones + zeros);
-        char digits[3];
-
-        digits[0] = (percent / 10) + '0';
-        digits[1] = (percent % 10) + '0';
-        digits[2] = 0;
-        Graphics_drawString(&g_sContext, (int8_t *) digits, -1, 20, 50, true);
-
+        if (i == 0 && OneShot1sTimerExpired()) {
+            i = ENTR_DELAY;
+            Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_BLUE);
+            Graphics_fillCircle(&g_sContext, x, y, 5);
+            Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_YELLOW);
+            x = randval;
+            randval = 64;
+            Graphics_fillCircle(&g_sContext, x, y, 5);
+            StartOneShot1sTimer();
+        } else if (i == 0) {
+        } else if (randBit) {
+            randval += 1 << i;
+            --i;
+        } else {
+            randval -= 1 << i;
+            --i;
+        }
     }
 
 }
