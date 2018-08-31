8c0c5ef289e9a4961359e6a52b4569f22ccb5935
Jacob Abel <jacobabel@live.com>
2018-08-30 21:15:22 -0400

    Interface

--- command_line_arguments.hpp
+++ command_line_arguments.hpp
@@ -1,2 +1,32 @@
+struct args {
+    std::string ifile;
+    std::string ofile;
+    std::map<std::string, bool> bool_args;
+    std::map<std::string, int> int_args;
+};
 
+class CommandLineArguments {
+private:
+    struct args arglist;
+public:    
+    //Parses the arguments
+    CommandLineArgumnts(int argc, char** argv);
+    
+    //Set the input file
+    setIFile(std::string const & dir);
+    
+    //Set the output file
+    setOFile(std::string const & dir);
 
+    //Add Boolean Arg
+    setBool(std::string const & param);
+
+    //Add Int Arg
+    setInt(std::string const & param);
+    
+    //Add String Arg
+    setString(std::string const & param);
+
+    //Retrieves the parsed argument struct
+    struct args const & arglist() const noexcept;
+};
