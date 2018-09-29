66b4af63fed2b169e03e4bff48c17177d8a78b42
jacoblambda <jacobabel@live.com>
2018-09-28 23:46:39 -0400

    Revert "Revert "Lambda""
    This reverts commit cb9711ec29f9408bcad17b48de2657e6dc25c247.

--- expression.cpp
+++ expression.cpp
@@ -149,10 +149,10 @@ Expression apply(const Atom& op, const std::vector<Expression>& args, const Envi
         Expression arg_expr;
         auto val = args.cbegin();
         for (auto arg = lambda_expr.tailConstBegin()->tailConstBegin(); 
-             arg != lambda_expr.tailConstBegin()->tailConstEnd();
+                 arg != lambda_expr.tailConstBegin()->tailConstEnd();
              ++arg, ++val) 
         {
-            lambda_env.add_exp(arg->head(), *val);
+            lambda_env.add_exp_force(arg->head(), *val);
         }
         
         return exp.eval(lambda_env);
@@ -248,12 +248,19 @@ Expression Expression::handle_lambda(Environment& env)
     }
 
     Expression expr(LAMBDA_KEYWORD);
-    if (m_tail[0].arg_length() <= 1) {
-        Expression e;
-        e.append(m_tail[0]);
+    Expression e;
+    if (m_tail[0].arg_length() > 0) {
+        e.append(Expression(m_tail[0].head()));
+        for (auto it = m_tail[0].tailConstBegin(); 
+                 it != m_tail[0].tailConstEnd();
+             ++it) 
+        {
+            e.append(it->head());
+        }
         expr.append(e);
     } else {
-        expr.append(m_tail[0]);
+        e.append(m_tail[0]);
+        expr.append(e);
     }
     expr.append(m_tail[1]);
     return expr;
