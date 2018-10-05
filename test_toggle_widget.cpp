439e609170d15b87b42145000d682ebecbf32073
Jacob Abel <jacobabel@live.com>
2018-10-05 01:03:56 -0400

    Add test

--- test_toggle_widget.cpp
+++ test_toggle_widget.cpp
@@ -8,13 +8,26 @@ class TestToggleWidget : public QObject {
   Q_OBJECT
 
 private slots:
-
-  // Define tests here
-  
+    void testRadio();
 };
 
-// Implement the tests here
-
+void TestToggleWidget::testRadio()
+{
+    ToggleWidget* widget = new ToggleWidget();
+    QRadioButton * rad = widget->findChild<QRadioButton *>();
+    QPushButton * but = widget->findChild<QPushButton *>();
+    QVERIFY(but != nullptr);
+    QVERIFY(rad != nullptr);
+    QVERIFY(!(widget->isOn()));
+    QVERIFY(!(rad->isChecked()));
+    QTest::mouseClick(but, Qt::LeftButton);
+    QVERIFY(widget->isOn());
+    QVERIFY(rad->isChecked());
+    QTest::mouseClick(but, Qt::LeftButton, Qt::NoModifier, but->rect().center());
+    QVERIFY(!(widget->isOn()));
+    QVERIFY(!(rad->isChecked()));
+    delete widget;
+}
 
 QTEST_MAIN(TestToggleWidget)
 #include "test_toggle_widget.moc"
