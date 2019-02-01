1d095d6f162ac17b0c097d5eda6d0726a18119a6
Patrick Schaumont <schaum@vt.edu>
2019-01-31 21:11:16 -0500

    homework 2 starter code

---
+++ compile.ps1
@@ -0,0 +1,7 @@
+vlib work
+vlog bitxmit.v
+vlog i2cmasterwrite.v
+vlog i2ctb.v
+vlog slave/i2cSlave.v
+vlog slave/registerInterface.v
+vlog slave/serialInterface.v
---
+++ i2cmasterwrite.v
@@ -0,0 +1,13 @@
+module i2cmasterwrite(input      clk,
+                      input  reset,
+                      input  init,
+                      output sck,
+                      inout  sda
+                      );
+   
+
+	// YOU HAVE TO DEVELOP THIS CODE
+    // YOU ARE ALLOWED (AND ENCOURAGED) TO INTRODUCE NEW FILES, NEW MODULES
+	// USE A HIERARCHICAL FSM, WITH BITXMIT THE FRONT-END
+   
+endmodule
