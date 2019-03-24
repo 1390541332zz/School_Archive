818ebc29d6dfdad23b6221bccc199a57482f7ac6
Jacob Abel <jacobabel@live.com>
2019-03-24 15:17:09 -0400

    testpattern

--- testpattern/XGA_testpattern.v
+++ testpattern/XGA_testpattern.v
@@ -9,11 +9,79 @@ module XGA_testpattern(
     output wire       VGA_BLANK_N,
     output wire       VGA_SYNC_N,
     output wire       VGA_HS,
-    output wire       VGA_VS);
-	
-	//---------- YOUR DESIGN STARTS HERE -----------------
-	// You can add code to this file, or add new verilog files.
-	// If you add new files, put all of them in the same directory
-	// as this file.
+    output wire       VGA_VS
+);
+
+/* Constant Time Functions */
+function integer log2(input integer x); begin
+    x = x - 1;
+    for (log2 = 0; x > 0; log2 = log2 + 1) x = x >> 1;
+end endfunction
+
+localparam 
+    width  = 1024, 
+    height = 768;
+
+wire
+    clk_75;
+wire [10:0]
+    ctr_h;
+wire [ 9:0]
+    ctr_v;
+reg [7:0]
+    vga_r,
+    vga_g,
+    vga_b;
+
+altpll_75 pll(
+    .refclk(CLOCK_50),
+    .rst(1'b0),
+    .outclk_0(clk_75),
+    /* verilator lint_off PINCONNECTEMPTY */
+    .locked()
+    /* verilator lint_on PINCONNECTEMPTY */
+);
+
+vga_driver #(
+    .vga_width(width),
+    .vga_height(height),
+    .color_depth( 8),
+    /* Horizontal Timing Properties */
+    .h_front_cnt(24),
+    .h_sync_cnt(136),
+    .h_back_cnt(144),
+    .pixel_cnt(   1),
+    /* Vertical Timing Properties */
+    .v_front_cnt( 3),
+    .v_sync_cnt(  6),
+    .v_back_cnt( 29),
+    .frame_cnt(   1)
+) xga (
+    .clk(clk_75),        
+    .vga_r_in(vga_r),
+    .vga_g_in(vga_g),
+    .vga_b_in(vga_b),
+    .vga_r_out(VGA_R),
+    .vga_g_out(VGA_G),
+    .vga_b_out(VGA_B),
+    .vga_clk(VGA_CLK),
+    .vga_blank_n(VGA_BLANK_N),
+    .vga_sync_n(VGA_SYNC_N),
+    .vga_hs(VGA_HS),
+    .vga_vs(VGA_VS),
+    .ctr_h(ctr_h),
+    .ctr_v(ctr_v)
+);
+
+always @(*) case (ctr_h >> 3)
+    0: {vga_r, vga_g, vga_b} = {8'h00, 8'h00, 8'h00};
+    1: {vga_r, vga_g, vga_b} = {8'hFF, 8'h00, 8'h00};
+    2: {vga_r, vga_g, vga_b} = {8'h00, 8'hFF, 8'h00};
+    3: {vga_r, vga_g, vga_b} = {8'hFF, 8'hFF, 8'h00};
+    4: {vga_r, vga_g, vga_b} = {8'h00, 8'h00, 8'hFF};
+    5: {vga_r, vga_g, vga_b} = {8'hFF, 8'h00, 8'hFF};
+    6: {vga_r, vga_g, vga_b} = {8'h00, 8'hFF, 8'hFF};
+    7: {vga_r, vga_g, vga_b} = {8'hFF, 8'hFF, 8'hFF};
+endcase
 
 endmodule
---
+++ testpattern/vga_driver.v
@@ -0,0 +1,105 @@
+module vga_driver #(
+    /* Display Properties */
+    parameter vga_width   = 1024,
+    parameter vga_height  = 768,
+    parameter color_depth = 8,
+
+    /* Horizontal Timing Properties */
+    parameter h_front_cnt = 24,
+    parameter h_sync_cnt  = 136,
+    parameter h_back_cnt  = 144,
+    parameter pixel_cnt   = 1,
+    
+    /* Vertical Timing Properties */
+    parameter v_front_cnt = 3,
+    parameter v_sync_cnt  = 6,
+    parameter v_back_cnt  = 29,
+    parameter frame_cnt   = 1,
+    
+    parameter h_addr_cnt  = pixel_cnt * vga_width,
+    parameter v_addr_cnt  = frame_cnt * vga_height,
+    
+    /* Horizontal Timing Triggers */
+    parameter h_front_start = 0,
+    parameter h_sync_start  = h_front_start + h_front_cnt, 
+    parameter h_back_start  = h_sync_start  + h_sync_cnt,
+    parameter h_addr_start  = h_back_start  + h_back_cnt,
+    parameter h_cnt         = h_addr_start  + h_addr_cnt,
+    
+    /* Vertical Timing Triggers */
+    parameter v_front_start = 0,
+    parameter v_sync_start  = v_front_start + v_front_cnt, 
+    parameter v_back_start  = v_sync_start  + v_sync_cnt,
+    parameter v_addr_start  = v_back_start  + v_back_cnt,
+    parameter v_cnt         = v_addr_start  + v_addr_cnt
+)(
+    input  wire                     clk,             
+    input  wire [color_depth - 1:0] vga_r_in,
+    input  wire [color_depth - 1:0] vga_g_in,
+    input  wire [color_depth - 1:0] vga_b_in,
+    output wire [color_depth - 1:0] vga_r_out,
+    output wire [color_depth - 1:0] vga_g_out,
+    output wire [color_depth - 1:0] vga_b_out,
+    output wire                     vga_clk,
+    output wire                     vga_blank_n,
+    output wire                     vga_sync_n,
+    output wire                     vga_hs,
+    output wire                     vga_vs,
+    output reg [log2(h_cnt) - 1:0]  ctr_h,
+    output reg [log2(v_cnt) - 1:0]  ctr_v
+);
+
+/* Constant Time Functions */
+function integer log2(input integer x); begin
+    x = x - 1;
+    for (log2 = 0; x > 0; log2 = log2 + 1) x = x >> 1;
+end endfunction
+
+/*---------------------------------------------------------------------------*/
+
+
+wire [log2(h_cnt) - 1:0]
+    ctr_h_next;
+wire [log2(v_cnt) - 1:0]
+    ctr_v_next;
+wire
+    /* Horizontal Ticks */
+    h_sync,
+//  h_back,
+    h_addr,
+//  h_front,
+
+    /* Vertical Ticks */
+    v_sync,
+//  v_back,
+//  v_front,
+    v_addr;
+
+/*---------------------------------------------------------------------------*/
+
+//assign h_front   = (ctr_h >= h_front_start) && (ctr_h < h_sync_start );
+assign h_sync      = (ctr_h >= h_sync_start ) && (ctr_h < h_back_start );
+//assign h_back    = (ctr_h >= h_back_start ) && (ctr_h < h_addr_start );
+assign h_addr      = (ctr_h >= h_addr_start ) && (ctr_h < h_cnt        );
+//assign v_front   = (ctr_v >= v_front_start) && (ctr_v < v_sync_start );
+assign v_sync      = (ctr_v >= v_sync_start ) && (ctr_v < v_back_start );
+//assign v_back    = (ctr_v >= v_back_start ) && (ctr_v < v_addr_start );
+assign v_addr      = (ctr_v >= v_addr_start ) && (ctr_v < v_cnt        );
+assign vga_clk     = clk;
+assign vga_r_out   = (h_addr) ? vga_r_in : 0;
+assign vga_g_out   = (h_addr) ? vga_g_in : 0;
+assign vga_b_out   = (h_addr) ? vga_b_in : 0;
+assign vga_hs      = h_sync;
+assign vga_vs      = v_sync;
+assign vga_sync_n  = vga_hs ~^ vga_vs;
+assign vga_blank_n = ~(v_addr | h_addr);
+
+assign ctr_h_next  = ctr_h + 1;
+assign ctr_v_next  = ctr_v + 1;
+
+always @(posedge clk) begin
+    ctr_h <= (ctr_h_next < h_cnt) ? ctr_h_next : 0;
+    ctr_v <= (ctr_v_next < v_cnt) ? ctr_v_next : 0;
+end
+
+endmodule
