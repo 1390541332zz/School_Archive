38b4e68b93002bcdea44ec995af9dab7f7688f5f
Jacob Abel <jacobabel@live.com>
2019-04-12 21:53:14 -0400

    Update

--- XGA_Graphics/ball_gen.v
+++ XGA_Graphics/ball_gen.v
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------*
- * Module: XGA Graphics                                                      *
- * Purpose: Top Level Module for an XGA bouncing ball screensaver graphic    *
- * Last Edit: 2019-04-02                                                     *
+ * Module: Ball Generator                                                    *
+ * Purpose: Object Controller for the Bouncing Ball Animation                *
+ * Last Edit: 2019-04-12                                                     *
  * Maintainer: Jacob Abel                                                    *
  *---------------------------------------------------------------------------*/
 
@@ -32,74 +32,49 @@ module ball_gen #(
 localparam 
     h_width = log2(width),
     v_width = log2(height);
-localparam [h_width - 1 : 0]
-    x_init  = init_x[h_width - 1 : 0];
-localparam [v_width - 1 : 0]
-    y_init  = init_y[v_width - 1 : 0];
 
-reg [h_width - 1:0]
+wire [h_width - 1:0]
     h_pos_next;
-reg [v_width - 1:0]
+wire [v_width - 1:0]
     v_pos_next;
 wire signed [h_width + 1:0]
-    h_min,
-    h_max;
+    x_next;
 wire signed [v_width + 1:0]
-    v_min,
-    v_max;
+    y_next;
 reg [1:0]
     h_vec,
     v_vec,
     h_vec_next,
     v_vec_next;
+wire
+    bounce_x,
+    bounce_y;
 
 /*---------------------------------------------------------------------------*/
 /*                                 Compute                                   */
 /*---------------------------------------------------------------------------*/
 
+assign x_next     = $signed({2'b0, h_pos}) + $signed({10'b0, h_vec});
+assign y_next     = $signed({2'b0, v_pos}) + $signed({10'b0, v_vec});
+
+assign h_pos_next = (move) ? x_next[h_width - 1 : 0] : h_pos;
+assign v_pos_next = (move) ? y_next[v_width - 1 : 0] : v_pos;
+
 /* verilator lint_off WIDTH */
-
-assign h_min = $signed(h_pos) - $signed(ball_radius) + (h_vec * $signed(init_vec_x));
-assign h_max = $signed(h_pos) + $signed(ball_radius) + (h_vec * $signed(init_vec_x));
-assign v_min = $signed(v_pos) - $signed(ball_radius) + (v_vec * $signed(init_vec_y));
-assign v_max = $signed(v_pos) + $signed(ball_radius) + (v_vec * $signed(init_vec_y));
-
-always @(*) if (move & (h_min <= 0)) begin
-    h_pos_next = 0;
-    h_vec_next = 2'sh1;
-end else if (move & (h_max >= (width - 1))) begin
-    h_pos_next = width - 1;
-    h_vec_next = -2'sh1;
-end else if (move) begin
-    h_pos_next = h_pos + (h_vec * $signed(init_vec_x));
-    h_vec_next = h_vec;
-end else begin
-    h_pos_next = h_pos;
-    h_vec_next = h_vec;
-end
-
-always @(*) if (move & (v_min <= 0)) begin
-    v_pos_next = 0;
-    v_vec_next = 2'sh1;
-end else if (move & (v_max >= (height - 1))) begin
-    v_pos_next = height - 1;
-    v_vec_next = -2'sh1;
-end else if (move) begin
-    v_pos_next = v_pos + (v_vec * $signed(init_vec_y));
-    v_vec_next = v_vec;
-end else begin
-    v_pos_next = v_pos;
-    v_vec_next = v_vec;
-end
-
+assign bounce_x   = (  (x_next[h_width - 1 : 0] <= ball_radius)
+                    && (x_next[h_width - 1 : 0] >= (width - ball_radius)));
+assign bounce_y   = (  (y_next[v_width - 1 : 0] <= ball_radius)
+                    && (y_next[v_width - 1 : 0] >= (height - ball_radius)));
 /* verilator lint_on WIDTH */
+
+assign h_vec_next = (move && bounce_x) ? (-h_vec) : h_vec; 
+assign v_vec_next = (move && bounce_y) ? (-v_vec) : v_vec; 
 
 always @(posedge clk) begin
     h_pos <= (reset) ? init_x[h_width - 1 : 0] : h_pos_next;
     v_pos <= (reset) ? init_y[v_width - 1 : 0] : v_pos_next;
-    h_vec <= (reset) ? 2'sh1                   : h_vec_next;
-    v_vec <= (reset) ? 2'sh1                   : v_vec_next;
+    h_vec <= (reset) ? init_vec_x[1:0]         : h_vec_next;
+    v_vec <= (reset) ? init_vec_y[1:0]         : v_vec_next;
 end
-
 
 endmodule
